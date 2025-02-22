4811f6a85acac4df057e7ceff4614cc5aa022d6c
keisuke-333 <keikubo.999@gmail.com>
2025-02-23 00:07:17 +0900

    feat: impl TodoUsecase

---
+++ internal/usecase/todo_usecase.go
@@ -0,0 +1,42 @@
+package usecase
+
+import (
+	"github.com/kei3dev/todo-app-api-go/internal/entity"
+	"github.com/kei3dev/todo-app-api-go/internal/repository"
+)
+
+type TodoUsecase interface {
+	CreateTodo(todo *entity.Todo) error
+	GetTodoByID(id uint) (*entity.Todo, error)
+	GetTodosByUserID(userID uint) ([]entity.Todo, error)
+	UpdateTodo(todo *entity.Todo) error
+	DeleteTodo(id uint) error
+}
+
+type todoUsecaseImpl struct {
+	todoRepo repository.TodoRepository
+}
+
+func NewTodoUsecase(todoRepo repository.TodoRepository) TodoUsecase {
+	return &todoUsecaseImpl{todoRepo: todoRepo}
+}
+
+func (u *todoUsecaseImpl) CreateTodo(todo *entity.Todo) error {
+	return u.todoRepo.Create(todo)
+}
+
+func (u *todoUsecaseImpl) GetTodoByID(id uint) (*entity.Todo, error) {
+	return u.todoRepo.FindByID(id)
+}
+
+func (u *todoUsecaseImpl) GetTodosByUserID(userID uint) ([]entity.Todo, error) {
+	return u.todoRepo.FindAllByUserID(userID)
+}
+
+func (u *todoUsecaseImpl) UpdateTodo(todo *entity.Todo) error {
+	return u.todoRepo.Update(todo)
+}
+
+func (u *todoUsecaseImpl) DeleteTodo(id uint) error {
+	return u.todoRepo.Delete(id)
+}
