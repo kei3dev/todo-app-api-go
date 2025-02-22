a89f885f479e7a7c64a6967309f96aece31c0068
keisuke-333 <keikubo.999@gmail.com>
2025-02-23 00:50:17 +0900

    feat: impl UserHandler

---
+++ internal/handler/user_handler.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/kei3dev/todo-app-api-go/internal/entity"
+	"github.com/kei3dev/todo-app-api-go/internal/usecase"
+)
+
+type UserHandler struct {
+	UserUsecase usecase.UserUsecase
+}
+
+func NewUserHandler(userUsecase usecase.UserUsecase) *UserHandler {
+	return &UserHandler{UserUsecase: userUsecase}
+}
+
+func (h *UserHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	var user entity.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	err := h.UserUsecase.RegisterUser(&user)
+	if err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
+
+func (h *UserHandler) GetUserByID(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	user, err := h.UserUsecase.GetUserByID(uint(id))
+	if err != nil {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
