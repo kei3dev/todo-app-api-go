a8354e585caa43bc0f800431977b07c5f3ae62b5
keisuke-333 <keikubo.999@gmail.com>
2025-03-17 13:04:08 +0900

    feat: add IRepository interface to repos

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -3,6 +3,7 @@ package repository
 import "github.com/kei3dev/todo-app-api-go/internal/entity"
 
 type UserRepository interface {
+	IRepository
 	Create(user *entity.User) error
 	FindByID(id uint) (*entity.User, error)
 	FindByEmail(email string) (*entity.User, error)
